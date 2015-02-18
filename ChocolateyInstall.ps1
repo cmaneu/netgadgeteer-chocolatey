db3c0cd4df46ae73dc93656cabf1d1e4b981e399
Christopher MANEU <christopher@maneu.net>
2015-02-18 17:08:40 +0100

    Add base files

---
+++ ChocolateyInstall.ps1
@@ -0,0 +1,6 @@
+$name   = 'NetGadgeteer'
+$url = 'https://gadgeteer.codeplex.com/downloads/get/918081'
+$url64 = 'https://gadgeteer.codeplex.com/downloads/get/918081'
+$silent = '/quiet'
+
+Install-ChocolateyPackage $name 'msi' $silent $url $url64
